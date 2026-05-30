5fbfcb9c1d701ba65b7fcc84e748893e7ae25b92
JCPrime01 <vanzokiller@gmail.com>
2026-05-30 09:21:32 -0300

    chore: shareCode 9SRPHK55KDD -> QM37GAS1NGI (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/61wz68wh?shareCode=9SRPHK55KDD&afp=oa' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/61wz68wh?shareCode=QM37GAS1NGI&afp=oa' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
