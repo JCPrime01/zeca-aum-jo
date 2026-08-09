e2d7151fe1d7151b10c23fc03682a23d856afb43
JCPrime01 <vanzokiller@gmail.com>
2026-08-09 10:03:54 -0300

    chore: shareCode VOA6GTMNQ81 -> YBAAYA1GOBQ (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/cj7zg19l?campaign_id=14473&shareCode=VOA6GTMNQ81&afp1=oa' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/cj7zg19l?campaign_id=14473&shareCode=YBAAYA1GOBQ&afp1=oa' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
